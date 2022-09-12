2c814c1c785f6f766b8d2e6bd2c02cc556c6f20b
samydoesit <sxburk@gmail.com>
2022-09-12 02:03:14 +0200

    feat: add turbo linting plugin

--- packages/config/eslint.cjs
+++ packages/config/eslint.cjs
@@ -3,5 +3,5 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 const { defineConfig } = require('eslint-define-config')
 
 module.exports = defineConfig({
-  extends: ['@nuxtjs/eslint-config-typescript']
+  extends: ['turbo', '@nuxtjs/eslint-config-typescript']
 })
